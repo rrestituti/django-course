ff22ea411615ebd5b983fde9306abf08df2be030
Renato Restituti <rrestituti@gmail.com>
2025-07-05 00:06:53 -0300

    feat: Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Renato Restituti',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse("Sobre")
