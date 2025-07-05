8b8cf1cc216fc2e68a2b61004bdcde2ba8ab31e8
Renato Restituti <rrestituti@gmail.com>
2025-07-04 23:49:34 -0300

    feat: adicionado templates

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Renato Restituti',
+    })
+
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+
+def sobre(request):
+    return HttpResponse("Sobre")
